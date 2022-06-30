7907ac2f731e33b77e3f868bae972b73b50192b5
A Z <alejo8708@gmail.com>
2022-06-30 15:46:14 +0200

    Final SQL

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -30,22 +30,26 @@ cartRouter.get('', (req, res, next) => {
 cartRouter.get('/products', (req, res) => {
     let cartAmount = 0;
 
-    cartSQLite.show()
+    return cartSQLite.show()
     .then( resp => {
         if ( resp.errno == 1) {
             res.redirect('/api/cart')
         } else {
             let cartList = [];
+            let cartQty = [];
             for (item of resp) {
                 cartList.push(item.productId)
-            }
+                cartQty.push(item.qty)
+            }                        
             return knex.select('*').from('products').whereIn('id', cartList)
             .then( resp => {
                 let cartListRender = resp;
-                cartListRender.forEach(item => {
-                    cartAmount += item.price;
+                cartListRender.forEach((item, index) => {
+                    item.qty = cartQty[index]
+                    cartAmount += item.price * item.qty;
+                    
                 })
-                cartAmount.toFixed(2);
+                cartAmount.toFixed(3);
                 res.render('cartList', { cartListRender, cartAmount })
             })            
         }
@@ -66,6 +70,19 @@ cartRouter.post('/products/:id', (req, res, next) => {
             })
         } else {
             let itemFilter = cartItems.filter( item => item.productId == req.params.id)
+            
+            if (itemFilter.length > 0) {
+                return cartSQLite.addQty(Number(req.params.id))
+                .then(_ => {
+                    console.log('Product Qty Added')
+                })
+            } else if (itemFilter.length === 0) {
+                return cartSQLite.addProduct(idp)
+                .then(_ => {
+                    console.log('Product Added')
+                })
+            }
+            
             console.log(itemFilter, req.params.id)
         }
     })
